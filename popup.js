8b30b52bbe424fb22209d1eb322064b05e6bbbd0
WaterAndBin <1336263157@qq.com>
2025-10-10 01:01:09 +0800

    feat: 添加热加载，完善加入黑名单逻辑

--- popup.js
+++ popup.js
@@ -1,16 +1,38 @@
-function findBlockList(callback) {
-  chrome.storage.local.get(["test"], (result) => {
+const key = "web_blockList";
+
+/**
+ * 查找黑名单
+ * @param {关键词} title_key string
+ * @param {黑名单地址} url string
+ * @param {回调函数} callback function
+ */
+function findBlockList(title_key, url, callback) {
+  chrome.storage.local.get([title_key], (result) => {
     if (result) {
-      callback(result.test);
+      const res = result[title_key];
+
+      if (!res) {
+        callback(result[title_key]);
+      } else {
+        // const blockUrl = res.find((items)=>)
+      }
     } else {
       console.error("暂未存储数据");
     }
   });
 }
 
-function setBlockList() {
-  chrome.storage.local.set({ test: "333" }, function (res) {
-    console.log("存储成功");
+/**
+ *
+ * @param {列表} list
+ */
+function setBlockList(list) {
+  chrome.storage.local.set({ web_blockList: list }, function (res) {
+    console.log("拉黑该网站成功");
+    // alert("拉黑该网站成功");
+    // setTimeout(() => {
+    //   chrome.tabs.reload({ bypassCache: false });
+    // }, 1000);
   });
 }
 
@@ -22,9 +44,12 @@ document.getElementById("pull").addEventListener("click", function () {
     const url = tabs?.[0]?.url;
     if (url) {
       console.log("当前页面 URL:", url);
-      setBlockList();
-      findBlockList((value) => {
-        console.log(value);
+      findBlockList(key, (value) => {
+        if (!value) {
+          setBlockList([url]);
+        } else {
+          // setBlockList([value]);
+        }
       });
     } else {
       alert("当前页面无法加入到黑名单当中！");
